67e18fe68c270c8d8726393bbec000ef84019eb6
honghan <honghan.wu@gmail.com>
2016-09-01 17:50:43 +0100

    multithreading pdf annotation and highlight extraction

---
+++ ann_utils.py
@@ -0,0 +1,45 @@
+from os import listdir
+from os.path import isfile, join
+import Queue
+import threading
+
+
+# list files in a folder and put them in to a queue for multi-threading processing
+def multi_thread_process_files(dir_path, file_extension, num_threads, process_func,
+                               proc_desc='processed', args=None):
+    onlyfiles = [f for f in listdir(dir_path) if isfile(join(dir_path, f))]
+    pdf_queque = Queue.Queue(len(onlyfiles))
+    num_pdfs = 0
+    for f in onlyfiles:
+        if f.endswith('.' + file_extension):
+            pdf_queque.put_nowait(join(dir_path, f))
+            num_pdfs += 1
+
+    thread_num = min(num_pdfs, num_threads)
+    arr = [process_func] if args is None else [process_func] + args
+    arr.insert(0, pdf_queque)
+    for i in range(thread_num):
+        t = threading.Thread(target=multi_thread_do, args=tuple(arr))
+        t.daemon = True
+        t.start()
+    pdf_queque.join()
+    print('{0} files {1}'.format(num_pdfs, proc_desc))
+
+
+def multi_thread_do(q, func, *args):
+    while True:
+        p = q.get()
+        func(p, *args)
+        q.task_done()
+
+
+def simple_do(f, t):
+    print("{}".format(f))
+
+
+def test():
+    multi_thread_process_files('/Users/jackey.wu/Documents/working/Alistair/fasion brain', 'docx', 3,
+                               simple_do, args=[' -'])
+
+if __name__ == "__main__":
+    test()
--- pdf_processor.py
+++ pdf_processor.py
@@ -3,63 +3,84 @@ import sys
 import PyQt4
 import json
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile, join, split
 import codecs
 import subprocess
-from os import chdir
+from os import chdir, rename, remove
+import Queue
+import threading
+import ann_utils as util
+
+# sapienta path
+sapienta_path = r'/home/ubuntu/Documents/sapienta'
+# max thread numbers for sapienta anntoation
+thread_num_sapienta = 10
+# max thread numbers for extracting highlights
+thread_num_highlights = 10
 
 
 # extract highlights from a PDF file
-def extract_pdf_hightlights(pdf_path):
-
-    doc = popplerqt4.Poppler.Document.load(pdf_path)
-    total_annotations = 0
-    ht = {}
-    for i in range(doc.numPages()):
-        page = doc.page(i)
-        annotations = page.annotations()
-        (pwidth, pheight) = (page.pageSize().width(), page.pageSize().height())
-        if len(annotations) > 0:
-            for annotation in annotations:
-                if isinstance(annotation, popplerqt4.Poppler.Annotation):
-                    total_annotations += 1
-                    if(isinstance(annotation, popplerqt4.Poppler.HighlightAnnotation)):
-                        quads = annotation.highlightQuads()
-                        txt = ""
-                        for quad in quads:
-                            rect = (quad.points[0].x() * pwidth,
-                                    quad.points[0].y() * pheight,
-                                    quad.points[2].x() * pwidth,
-                                    quad.points[2].y() * pheight)
-                            bdy = PyQt4.QtCore.QRectF()
-                            bdy.setCoords(*rect)
-                            txt = txt + unicode(page.text(bdy)) + ' '
-                        key = str(i+1)
-                        if key in ht:
-                            ht[key].append(txt)
-                        else:
-                            ht[key] = [txt]
-    return ht
-
-
-
-def process_pdf(path):
-    onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
-    for f in onlyfiles:
-        if f.endswith('.pdf'):
-            ht = extract_pdf_hightlights(join(path,  f))
-            with codecs.open(join(path, f[:f.find('.')] + '_ht.json'), 'w', encoding='utf-8') as write_file:
-                json.dump(ht, write_file)
+def extract_pdf_highlights(pdf_path, output_path):
+        doc = popplerqt4.Poppler.Document.load(pdf_path)
+        total_annotations = 0
+        ht = {}
+        for i in range(doc.numPages()):
+            page = doc.page(i)
+            annotations = page.annotations()
+            (pwidth, pheight) = (page.pageSize().width(), page.pageSize().height())
+            if len(annotations) > 0:
+                for annotation in annotations:
+                    if isinstance(annotation, popplerqt4.Poppler.Annotation):
+                        total_annotations += 1
+                        if(isinstance(annotation, popplerqt4.Poppler.HighlightAnnotation)):
+                            quads = annotation.highlightQuads()
+                            txt = ""
+                            for quad in quads:
+                                rect = (quad.points[0].x() * pwidth,
+                                        quad.points[0].y() * pheight,
+                                        quad.points[2].x() * pwidth,
+                                        quad.points[2].y() * pheight)
+                                bdy = PyQt4.QtCore.QRectF()
+                                bdy.setCoords(*rect)
+                                txt = txt + unicode(page.text(bdy)) + ' '
+                            key = str(i+1)
+                            if key in ht:
+                                ht[key].append(txt)
+                            else:
+                                ht[key] = [txt]
+        with codecs.open(join(output_path, pdf_path[:pdf_path.find('.')] + '_ht.json'),
+                         'w', encoding='utf-8') as write_file:
+            json.dump(ht, write_file)
 
 
-def sapienta_annotate(pdf_path):
-    chdir(r'/home/ubuntu/Documents/sapienta')
-    # subprocess.Popen(["pdfxconv", "-a", pdf_path], cwd=r'/home/ubuntu/Documents/sapienta')
+def sapienta_annotate(pdf_path, output_path):
+    chdir(sapienta_path)
     proc = subprocess.Popen(["pdfxconv", "-a", pdf_path], stdout=subprocess.PIPE)
     streamdata = proc.communicate()[0]
     rc = proc.returncode
-    print('return code:', rc)
+    if rc != 0:
+        print('failed to annotate {0} with Sapienta'.format(pdf_path))
+    else:
+        head, tail = split(pdf_path)
+        fname = tail[:tail.rfind('.')]
+        ann_file_name = fname + '_annotated.xml'
+        rename(join(head, ann_file_name), join(output_path, ann_file_name))
+        remove(join(head, fname + '.xml'))
+
+
+def sapienta_process(pdf_dir_path, opath):
+    util.multi_thread_process_files(pdf_dir_path, 'pdf', thread_num_sapienta, sapienta_annotate,
+                                    proc_desc='annotated by Sapienta',
+                                    args=[opath])
+
+
+def extract_highlights_process(pdf_dir_path, opath):
+    util.multi_thread_process_files(pdf_dir_path, 'pdf', thread_num_highlights, extract_pdf_highlights,
+                                    proc_desc='extracted highlights',
+                                    args=[opath])
 
 if __name__ == "__main__":
-    # process_pdf(sys.argv[1])
-    sapienta_annotate('t.pdf')
+    pdf_file_path = sys.argv[1]
+    output_path = sys.argv[2]
+    sapienta_process(pdf_file_path, output_path)
+    extract_highlights_process(pdf_file_path, output_path)
