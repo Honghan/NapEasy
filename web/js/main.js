75e60b025a82c4db68edf82b36ec552089e12094
honghan <honghan.wu@gmail.com>
2016-09-09 01:11:01 +0100

    updated web files

--- web/js/main.js
+++ web/js/main.js
@@ -1,7 +1,16 @@
 (function($){
-  $(document).ready(function(){
-    console.log('start loading...')
-    tmining.load_text("Ahn%20et%20al.,%20(2011)%20-%20The%20cortical%20neuroanatomy%20of%20neuropsychological%20deficits%20in%20MCI%20and%20AD_annotated_ann.sum");
-  })
+	var files = ["Ahn et al., (2011) - The cortical neuroanatomy of neuropsychological deficits in MCI and AD_annotated_ann_scores.json","Ahn et al., (2011) - The cortical neuroanatomy of neuropsychological deficits in MCI and AD_annotated_ann.sum","Allert et al., (2011) - Role of dysphagia in evaluating PD for STN-DBS._annotated_ann.sum","Altug et al., (2011) - The influence of subthalamic nucleus DBS on daily living activities in PD_annotated_ann.sum","Altug et al., (2014) - Brain stimulation of subthalamic nucleus surgery on gait and balance in PD_annotated_ann.sum","Arnaldi et al., (2016) - Functional neuroimaging and clinical features of drug naive patients with de novo PD and RBD_annotated_ann.sum","Avila et al., (2011) - Effect of temporal lobe structure volume on memory in eldery depressed patients_annotated_ann.sum","Bartova et al., (2010) - Correlation between substantia nigra features detected by sonography and PD_annotated_ann.sum","Benninger et al., (2008) - Morphological differences in PD with and without rest tremor_annotated_ann.sum"]
+	$(document).ready(function(){
+		console.log('start loading...')
+		for(var i=0;i<files.length;i++){
+			$('#file_list').append($("<option></option>")
+	                    .attr("value",files[i])
+	                    .text(decodeURIComponent(files[i]))); 
+		}
+
+		$('#file_list').on('change', function() {
+			tmining.load_text(this.value);
+		});
+	})
 
 })(this.jQuery)
--- web/js/tmining.js
+++ web/js/tmining.js
@@ -3,14 +3,19 @@
 	if(typeof tmining == "undefined") {
 		tmining = {
 			server_prefix: "http://honghan.info/pn-tmining/summaries/",
-
+			cur_file: "",
 			load_text: function(file_path){
-				var url = tmining.server_prefix + file_path
-				console.log(url)
-				$('#loadDiv').load(url, function(){
-					eval("var summ = " + $('#loadDiv').html());
-					console.log(summ);
-				});
+				tmining.cur_file = file_path;
+				// var url = tmining.server_prefix + file_path;
+				// console.log(url)
+				// $('#loadDiv').load(url, function(){
+				// 	eval("var summ = " + $('#loadDiv').html());
+				// 	console.log(summ);
+				// 	tmining.render_summary(summ);
+				// });
+				eval("var summ = " + $('#loadDiv').html());
+				console.log(summ);
+				tmining.render_summary(summ);
 			},
 
 			load_script: function(file_path, method, sendData, success, error){
@@ -30,6 +35,27 @@
 			load_cb:function(data, textStatus, jqXHR){
 				console.log("callback...")
 				console.log(data)
+			},
+
+			render_summary: function(summ){
+				$('.title').html(decodeURIComponent(tmining.cur_file));
+				var s = '';
+				if (summ.hasOwnProperty('goal'))
+					s += tmining.render_typed_sentence('goal', summ.goal);
+				if (summ.hasOwnProperty('method'))
+					s += tmining.render_typed_sentence('method', summ.method);
+				if (summ.hasOwnProperty('findings'))
+					s += tmining.render_typed_sentence('findings', summ.findings);
+				if (summ.hasOwnProperty('general'))
+					s += tmining.render_typed_sentence('general', summ.findings);
+				$('.summary').html(s);
+			},
+
+			render_typed_sentence: function(type, sentences){
+				var s = "<div class='stype'>" + type + "</div>";
+				for(var i=0;i<sentences.length;i++)
+					s += "<div class='sentence'><span class='sid'>" + sentences[i][1].sid + ".</span>" + sentences[i][0] + "</div>"
+				return s;
 			}
 		}
 	}
